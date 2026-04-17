d91e3078ef9398d0bbce8a8e0c939a8fb5abd9ba
lokyus <lokyus@gmail.com>
2026-04-17 09:47:46 -0300

    Adicionado sistema de classes baseada em atributos

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 function rolarDados() {
-    // Gera um número aleatório entre 1 e 20 (tipo um D20 de RPG)
+    // 1. Gerando os atributos (Lógica básica)
     let forca = Math.floor(Math.random() * 20) + 1;
     let agilidade = Math.floor(Math.random() * 20) + 1;
+    let classe = "";
 
+    // 2. Tomada de decisão (Aqui entra o ADS!)
+    if (forca > agilidade && forca >= 15) {
+        classe = "Guerreiro Pesado 🛡️";
+    } else if (agilidade > forca && agilidade >= 15) {
+        classe = "Ranger Veloz 🏹";
+    } else if (forca < 5 && agilidade < 5) {
+        classe = "Aldeão Comum 🥖";
+    } else {
+        classe = "Aventureiro Iniciante ⚔️";
+    }
+
+    // 3. Exibindo no HTML
     document.getElementById("forca").innerText = forca;
     document.getElementById("agilidade").innerText = agilidade;
-    console.log("Sistema de RPG inicializado com sucesso!");
+    document.getElementById("classe").innerText = classe; // Vamos precisar criar esse ID no HTML
 }
